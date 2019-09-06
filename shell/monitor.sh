5f3ddf75f3dfdf3dd2920e45b2de1ae1743193e6
linmin <linmin3@yy.com>
2019-09-07 00:34:11 +0800

    1

--- shell/monitor.sh
+++ shell/monitor.sh
@@ -1,14 +1,14 @@
-#寻找最大磁盘,判断是否大于10GB
+#寻找最大磁盘,判断是否大于200GB
 MAX_SIZE=`df | sed -n '2,$p' | sort -n -r -k 2  | sed -n '1,1p' | awk '{print $2}'`
 let "MAX_SIZE = MAX_SIZE / 1024" 
 echo "max size:$MAX_SIZE MB"
-if [ $MAX_SIZE -lt 10240 ]
+if [ $MAX_SIZE -lt 204800 ]
 then 
-	echo 'small than 10240 MB'
+	echo 'small than 204800 MB'
 	export DISK_MOUNTED=false
 	exit 1
 fi
-echo 'bigger than 10240 MB'
+echo 'bigger than 204800 MB'
 DISK_MOUNTED=true
 
 #获得最大磁盘UUID
