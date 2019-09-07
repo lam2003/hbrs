8a225c253af242431e6789228f4b8e50badce548
l <linmin093@gmail.com>
2019-09-07 09:51:29 +0800

    Update monitor.sh

--- shell/monitor.sh
+++ shell/monitor.sh
@@ -5,11 +5,8 @@ echo "max size:$MAX_SIZE MB"
 if [ $MAX_SIZE -lt 204800 ]
 then 
 	echo 'small than 204800 MB'
-	export DISK_MOUNTED=false
-	exit 1
 fi
 echo 'bigger than 204800 MB'
-DISK_MOUNTED=true
 
 #获得最大磁盘UUID
 DISK_DEV=`df | sed -n '2,$p' | sort -n -r -k 2  | sed -n '1,1p' | awk '{print $1}'`
@@ -19,8 +16,8 @@ echo $RS_UUID
 #磁盘剩余空间监控
 while : 
 do
-	TEMP=`df  | sed -n '2,$p' | sort -n  -k 2   | sed -n '1,1p' | awk '{print $4}'`
-	let "REST_SIZE = TEMP / 1024"
+	REST_SIZE=`df  | sed -n '2,$p' | sort -n -r -k 2   | sed -n '1,1p' | awk '{print $4}'`
+	let "REST_SIZE = REST_SIZE / 1024"
 	echo "$REST_SIZE MB"
 	sleep 1
 done
