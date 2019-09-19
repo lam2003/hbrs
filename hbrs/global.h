60467b0ea36f5f069f403275d280a55867f025bb
linmin <linmin3@yy.com>
2019-09-19 13:31:40 +0800

    1

--- hbrs/global.h
+++ hbrs/global.h
@@ -98,7 +98,7 @@
 #define RS_SLAVE_SDI_BASE 1                             //从片SDI通道起始偏移
 #define RS_SLAVE_SDI_NUM 4                              //从片SDI通道的数量
 
-#define RS_VERSION "v2.0.3"
+#define RS_VERSION "v2.0.5"
 
 #define RS_ASSERT(cond)     \
     while (!(cond))         \
